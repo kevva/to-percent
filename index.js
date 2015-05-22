b787e7d0ed390fb438814ffb9afed69e5f9c2c9d
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-23 00:06:44 +0200

    Change wording

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = function (val) {
 	if (typeof val !== 'number' || val < 0 || val > 1) {
-		throw new Error('Expected a value between 0 and 1');
+		throw new Error('Expected a number between 0 and 1');
 	}
 
 	return Math.round(val * 100);
