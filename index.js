67aa846a46cdfe49ebdf66b676bf01819c251861
Kevin Martensson <kevinmartensson@gmail.com>
2017-06-07 14:15:26 +0200

    ES2015ify and support Node.js >=4

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
+module.exports = val => {
+	if (typeof val !== 'number') {
+		throw new TypeError(`Expected a \`number\`, got \`${typeof val}\``);
+	}
 
-module.exports = function (val) {
-	if (typeof val !== 'number' || val < 0 || val > 1) {
-		throw new Error('Expected a number between 0 and 1');
+	if (val < 0 || val > 1) {
+		throw new TypeError(`Expected a \`number\` between 0 and 1, got \`${val}\``);
 	}
 
 	return Math.round(val * 100);
--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 import test from 'ava';
 import m from '.';
 
-test(t => {
+test('convert to percent', t => {
 	t.is(m(0.75), 75);
 	t.is(m(0.1256666), 13);
 	t.is(m(0.1246666), 12);
+});
+
+test('accepts a number between 0 and 1', t => {
+	t.throws(m.bind(null, '1'), 'Expected a `number`, got `string`');
+	t.throws(m.bind(null, 1.5), 'Expected a `number` between 0 and 1, got `1.5`');
 });
